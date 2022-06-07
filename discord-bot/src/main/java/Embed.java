b301c22a6d182b85552b040cfd8bc362b3892e03
KajorSuchodolski <230049@edu.p.lodz.pl>
2022-06-07 17:35:43 +0200

    Added new class for Embed object

---
+++ discord-bot/src/main/java/Embed.java
@@ -0,0 +1,13 @@
+import net.dv8tion.jda.api.EmbedBuilder;
+
+import java.awt.*;
+
+public class Embed {
+
+    public Embed() {
+        EmbedBuilder eb = new EmbedBuilder();
+        eb.setColor(Color.BLUE);
+        eb.setAuthor("Author");
+
+    }
+}
